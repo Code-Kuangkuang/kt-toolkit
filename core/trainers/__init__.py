2b10b44ff6d5a7e36b1571560c156cf375effbbf
kyrie <ky1057709392@gamil.com>
2026-02-08 14:40:11 +0800

    add dkvmn and akt model

--- core/trainers/__init__.py
+++ core/trainers/__init__.py
@@ -1,2 +1,3 @@
 from .dkt_trainer import DKTTrainer  # noqa: F401
 from .dkvmn_trainer import DKVMNTrainer  # noqa: F401
+from .akt_trainer import AKTTrainer  # noqa: F401
--- models/dkvmn.py
+++ models/dkvmn.py
@@ -10,6 +10,16 @@ from core.registry import MODEL_REGISTRY
 
 @MODEL_REGISTRY.register("dkvmn")
 class DKVMN(Module):
+    r""" DKVMN模型初始化
+    Args:
+        num_c: 概念数量
+        dim_s: 嵌入向量(知识状态)维度, 默认200
+        size_m: 记忆网络大小, 默认50
+        dropout: Dropout概率
+        emb_type: 题目嵌入类型，默认为"qid"
+        emb_path: 预训练题目嵌入路径，默认为空
+        pretrain_dim: 预训练题目嵌入维度, 默认为768
+    """
     def __init__(self, num_c, dim_s, size_m, dropout=0.2, emb_type='qid', emb_path="", pretrain_dim=768):
         super().__init__()
         self.model_name = "dkvmn"
@@ -17,10 +27,11 @@ class DKVMN(Module):
         self.dim_s = dim_s
         self.size_m = size_m
         self.emb_type = emb_type
-
         if emb_type.startswith("qid"):
             self.k_emb_layer = Embedding(self.num_c, self.dim_s)
+            # 键记忆矩阵：存储知识概念的静态表示
             self.Mk = Parameter(torch.Tensor(self.size_m, self.dim_s))
+            # 值记忆矩阵的初始值，存储知识概念的动态表示
             self.Mv0 = Parameter(torch.Tensor(self.size_m, self.dim_s))
 
         kaiming_normal_(self.Mk)
@@ -42,7 +53,7 @@ class DKVMN(Module):
             x = q + self.num_c * r
             k = self.k_emb_layer(q)
             v = self.v_emb_layer(x)
-        
+
         Mvt = self.Mv0.unsqueeze(0).repeat(batch_size, 1, 1)
 
         Mv = [Mvt]
@@ -50,6 +61,7 @@ class DKVMN(Module):
         w = torch.softmax(torch.matmul(k, self.Mk.T), dim=-1)
 
         # Write Process
+        # erase 向量 和 add 向量
         e = torch.sigmoid(self.e_layer(v))
         a = torch.tanh(self.a_layer(v))
 
