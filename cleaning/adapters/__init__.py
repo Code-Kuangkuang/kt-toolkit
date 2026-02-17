2e42dd3d65e0e968be5b0b73ef0fb3290b0107e3
kuang yi <1057709392@qq.com>
2026-02-17 17:23:10 +0800

    add algebra2005 process

--- cleaning/adapters/__init__.py
+++ cleaning/adapters/__init__.py
@@ -1,5 +1,9 @@
 from .assist2009 import run as assist2009
+from .assist2017 import run as assist2017
+from .algebra2005 import run as algebra2005
 
 ADAPTERS = {
     "assist2009": assist2009,
+    "assist2017": assist2017,
+    "algebra2005": algebra2005,
 }
